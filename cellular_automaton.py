6aed1c7e8720195ef3140d42945fbdaf4c5f9d8d
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-28 12:16:29 +0200

    randomizer

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -147,7 +147,30 @@ class CellularAutomaton2D:
 
 
 if __name__ == "__main__":
-    ca = CellularAutomaton2D((10, 10), (5, 5))
-    ca.initialize([(0, 0), (9, 9)], [(4, 4), (6, 6)])
+    grid_size = (25, 40)
+    nr_pedestrians = 10
+    nr_obstacles = 15
+
+    exit_pos = (grid_size[0] - 1, grid_size[1] - 1)
+    ca = CellularAutomaton2D(grid_size, exit_pos)
+
+    pedestrians = [
+        (np.random.randint(0, grid_size[0]), np.random.randint(0, grid_size[1]))
+        for _ in range(nr_pedestrians)
+    ]
+
+    obstacles = []
+    for _ in range(nr_obstacles):
+        while True:
+            obstacle_pos = (
+                np.random.randint(0, grid_size[0]),
+                np.random.randint(0, grid_size[1]),
+            )
+            if obstacle_pos not in pedestrians:
+                obstacles.append(obstacle_pos)
+                break  # Found a valid position, break the while loop
+
+    ca.initialize(pedestrians, obstacles)
+
     vis = PygameVisualizer(ca, fps=1)
     vis.visualize(50)
